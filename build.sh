ba8305f9c99254dee0363a99f212f70d99da2669
Andrea Falco <fox91fox@gmail.com>
2016-02-09 15:09:29 +0100

    PHP 5.6.18

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@ docker pull fox91/php:latest
 docker pull fox91/php:fpm
 
 # docker build -t fox91/php:latest ./
-docker tag -f fox91/php:latest docker.io/fox91/php:5.6.17-cli
+docker tag -f fox91/php:latest docker.io/fox91/php:5.6.18-cli
 docker tag -f fox91/php:latest docker.io/fox91/php:5.6-cli
 docker tag -f fox91/php:latest docker.io/fox91/php:5-cli
 docker tag -f fox91/php:latest docker.io/fox91/php:cli
-docker tag -f fox91/php:latest docker.io/fox91/php:5.6.17
+docker tag -f fox91/php:latest docker.io/fox91/php:5.6.18
 docker tag -f fox91/php:latest docker.io/fox91/php:5.6
 docker tag -f fox91/php:latest docker.io/fox91/php:5
 
 # docker build -t fox91/php:fpm ./
-docker tag -f fox91/php:fpm docker.io/fox91/php:5.6.17-fpm
+docker tag -f fox91/php:fpm docker.io/fox91/php:5.6.18-fpm
 docker tag -f fox91/php:fpm docker.io/fox91/php:5.6-fpm
 docker tag -f fox91/php:fpm docker.io/fox91/php:5-fpm
--- clean.sh
+++ clean.sh
@@ -5,14 +5,14 @@ set -x
 
 # fox91/php:cli
 docker rmi fox91/php:latest
-docker rmi fox91/php:5.6.17-cli
+docker rmi fox91/php:5.6.18-cli
 docker rmi fox91/php:5-cli
 docker rmi fox91/php:cli
-docker rmi fox91/php:5.6.17
+docker rmi fox91/php:5.6.18
 docker rmi fox91/php:5.6
 docker rmi fox91/php:5
 
 # fox91/php:fpm
 docker rmi fox91/php:fpm
-docker rmi fox91/php:5.6.17-fpm
+docker rmi fox91/php:5.6.18-fpm
 docker rmi fox91/php:5-fpm
