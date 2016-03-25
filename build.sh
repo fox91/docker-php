d912a1466627e3c9efca159b48c4559ed301b794
Andrea Falco <fox91fox@gmail.com>
2016-03-25 11:36:03 +0100

    Updated utility

--- build.sh
+++ build.sh
@@ -5,17 +5,19 @@ set -x
 
 
 docker pull fox91/php:latest
+IMAGE_ID=$(docker images -q fox91/php:latest)
 # docker build -t fox91/php:latest ./
-docker tag fox91/php:latest docker.io/fox91/php:5.6.19-cli
-docker tag fox91/php:latest docker.io/fox91/php:5.6-cli
-docker tag fox91/php:latest docker.io/fox91/php:5-cli
-docker tag fox91/php:latest docker.io/fox91/php:cli
-docker tag fox91/php:latest docker.io/fox91/php:5.6.19
-docker tag fox91/php:latest docker.io/fox91/php:5.6
-docker tag fox91/php:latest docker.io/fox91/php:5
+docker tag "${IMAGE_ID}" fox91/php:5.6.19-cli
+docker tag "${IMAGE_ID}" fox91/php:5.6-cli
+docker tag "${IMAGE_ID}" fox91/php:5-cli
+docker tag "${IMAGE_ID}" fox91/php:cli
+docker tag "${IMAGE_ID}" fox91/php:5.6.19
+docker tag "${IMAGE_ID}" fox91/php:5.6
+docker tag "${IMAGE_ID}" fox91/php:5
 
 docker pull fox91/php:fpm
+IMAGE_ID=$(docker images -q fox91/php:fpm)
 # docker build -t fox91/php:fpm ./fpm
-docker tag fox91/php:fpm docker.io/fox91/php:5.6.19-fpm
-docker tag fox91/php:fpm docker.io/fox91/php:5.6-fpm
-docker tag fox91/php:fpm docker.io/fox91/php:5-fpm
+docker tag "${IMAGE_ID}" fox91/php:5.6.19-fpm
+docker tag "${IMAGE_ID}" fox91/php:5.6-fpm
+docker tag "${IMAGE_ID}" fox91/php:5-fpm
