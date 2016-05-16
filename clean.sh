20e32073d41b2907347e5f4d96c160dc2c00da01
Andrea Falco <fox91fox@gmail.com>
2016-05-16 12:32:37 +0200

    PHP 5.6.21

--- clean.sh
+++ clean.sh
@@ -5,14 +5,14 @@ set -x
 
 # fox91/php:cli
 docker rmi fox91/php:latest
-docker rmi fox91/php:5.6.19-cli
+docker rmi fox91/php:5.6.21-cli
 docker rmi fox91/php:5-cli
 docker rmi fox91/php:cli
-docker rmi fox91/php:5.6.19
+docker rmi fox91/php:5.6.21
 docker rmi fox91/php:5.6
 docker rmi fox91/php:5
 
 # fox91/php:fpm
 docker rmi fox91/php:fpm
-docker rmi fox91/php:5.6.19-fpm
+docker rmi fox91/php:5.6.21-fpm
 docker rmi fox91/php:5-fpm
